d507b97595dd0ff08fd2fe5fadb645b4b2474118
Matthias Thoemmes <git@gmx.org>
2016-03-13 15:14:35 +0100

    Bumping to 0.2.7

--- build/jshint.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (gulp, config) {
-  gulp.task('jshint',function () {
-    var jshint = require('gulp-jshint');
-    return gulp.src(config.paths.jshint)
-    .pipe(jshint(config.jshint))
-    .pipe(jshint.reporter('default'));
-  });
-};
